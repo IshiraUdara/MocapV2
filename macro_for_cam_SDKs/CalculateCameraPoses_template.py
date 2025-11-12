e12e3b223be13119cc574790974e1f0d2acc19d6
Ishira Udara <ishira.20221603@iit.ac.lk>
2025-11-12 14:39:26 +0530

    assigned new floor images from Kinect cams

--- macro_for_cam_SDKs/CalculateCameraPoses_template.py
+++ macro_for_cam_SDKs/CalculateCameraPoses_template.py
@@ -177,8 +177,8 @@ def calculate_extrinsics(image_points):
         "t": np.array([[0],[0],[0]], dtype=np.float32)
     }]
 
-    image1 = cv.imread("macro_for_cam_SDKs/Kinect_x_Kinect_OLD/tracking/cam0/1761215118.png")
-    image2 = cv.imread("macro_for_cam_SDKs/Kinect_x_Kinect_OLD/tracking/cam1/1761215118.png")
+    image1 = cv.imread("macro_for_cam_SDKs/Kinect_Azure_x_Kinect_OLD/tracking/cam0/1762850218.png")
+    image2 = cv.imread("macro_for_cam_SDKs/Kinect_Azure_x_Kinect_OLD/tracking/cam1/1762850218.png")
 
     camera_poses = [{
         "R": np.eye(3),
